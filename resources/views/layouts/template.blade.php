fe460519b8cd18cb4f23e7b67a876ac6abf28423
Steffe Pans <r0831311@student.thomasmore.be>
2021-10-11 10:30:07 +0200

    lesson 11-10

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
     <title>@yield('title', 'The Vinyl Shop')</title>
 </head>
 <body>
@@ -12,7 +12,6 @@
     @yield('main', 'Page under construction ...')
 </main>
 @include('shared.footer')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
